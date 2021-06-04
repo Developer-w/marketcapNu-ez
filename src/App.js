ff2d0c8805f239978c3692d38a9012b0b42a9544
Developer-w <developerwb@protonmail.com>
2021-06-03 22:13:37 -0400

    Primeros pasos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg='colorNabVAr' variant='dark' fixed="top">
+        <Navbar.Brand>
+          <img src={"logo"} />{' '}
+          Logo
+        </Navbar.Brand>
+      </Navbar>
+      <div className="content">
+        Aqui ira el contenido del Header
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,6 @@
+{/* <Navbar bg='colorNabVAr' variant='dark' fixed="top">
+        <Navbar.Brand>
+          <img src={"logo"} />{' '}
+          Logo
+        </Navbar.Brand>
+</Navbar> */}
